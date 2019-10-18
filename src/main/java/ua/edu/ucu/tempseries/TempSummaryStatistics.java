e260100b2f47b86c50ba7523f96ac7d1a22d0ccd
markoburda <burda@ucu.edu.ua>
2019-10-18 17:43:53 +0300

    Initial commit

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,5 +1,31 @@
 package ua.edu.ucu.tempseries;
 
 public class TempSummaryStatistics {
-    
+    private double avgTemp;
+    private double devTemp;
+    private double minTemp;
+    private double maxTemp;
+
+    public TempSummaryStatistics(TemperatureSeriesAnalysis obj){
+        this.avgTemp = obj.average();
+        this.devTemp = obj.deviation();
+        this.minTemp = obj.min();
+        this.maxTemp = obj.max();
+    }
+
+    public double getAvgTemp() {
+        return avgTemp;
+    }
+
+    public double getDevTemp() {
+        return devTemp;
+    }
+
+    public double getMaxTemp() {
+        return maxTemp;
+    }
+
+    public double getMinTemp() {
+        return minTemp;
+    }
 }
--- src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
@@ -1,50 +1,122 @@
 package ua.edu.ucu.tempseries;
+import Math
 
 public class TemperatureSeriesAnalysis {
+
+    private double[]  templst;
+    private final double templim = -273;
 
     public TemperatureSeriesAnalysis() {
 
     }
 
     public TemperatureSeriesAnalysis(double[] temperatureSeries) {
-
+        this.setTemperatureSeries(temperatureSeries);
     }
 
     public double average() {
-        return -1;
+        int sum = 0;
+        if(this.templst.length == 0){
+            throw new IllegalArgumentException("List is empty");
+        }
+        for(int i : this.templst){
+            sum += i;
+        }
+        return sum/(double) this.templst.length;
     }
 
     public double deviation() {
-        return 0;
+        if(this.templst.length == 0){
+            throw new IllegalArgumentException("List is empty");
+        avg = average(this.templst);
+        double sum = 0.0;
+        for(int i : this.templst){
+            sum +=  (i  - avg)**2;;
+        }
+        double variance = sum/double(this.templst.length) - 1;
+        return variance**0.5;
     }
 
     public double min() {
-        return 0;
+        if(this.templst.length == 0){
+            throw new IllegalArgumentException("List is empty");
+        }
+        int minnum = 10**8;
+        for(int i : this.templst){
+            if (i < minnum){
+                minnum = i
+            }
+        }
+        return minnum;
     }
 
     public double max() {
-        return 0;
+        int maxnum = 0;
+        for(int i : this.templst){
+            if (i > maxnum){
+                maxnum = i;
+            }
+        }
+        return maxnum;
     }
 
     public double findTempClosestToZero() {
-        return 0;
+            if(tempValue.length == 0){
+                throw new IllegalArgumentException("List is empty");
+            }
+            int closestnum = 10*8;
+            for(int i : tempValue){
+                if Math.abs(i)  < closestnum{
+                    closestnum = Math.abs(i);
+                }
+            }
+            return closestnum;
     }
 
     public double findTempClosestToValue(double tempValue) {
-        return 0;
+            if(templst.length == 0){
+                throw new IllegalArgumentException("List is empty");
+            }
+            int closestnum = 10*8;
+            for(int i : this.templst){
+                if(Math.abs(i - tempValue)  < closestnum){
+                    closestnum = Math.abs(i);
+                }
+            }
+            return closestnum;
     }
 
     public double[] findTempsLessThen(double tempValue) {
-        return null;
+            List lessthan = new ArrayList();
+            for(int i : this.templst){
+                if(i < tempValue){
+                    lessthan.add(i);
+                }
+            }
+            return lessthan;
     }
 
     public double[] findTempsGreaterThen(double tempValue) {
-        return null;
+            List morethan = new ArrayList();
+            for(int i : this.templst){
+                if(i > tempValue){
+                    morethan.add(i);
+                }
+            }
+            return morethan;
     }
 
     public TempSummaryStatistics summaryStatistics() {
         return null;
     }
+
+    public expand(){
+            int n = this.templst.length * 2
+            double newtemplst = new double[n];
+            System.arraycopy(this.templst, 0, newtemplst, 0,  n / 2);
+            deftemplst(newtemplst);
+
+        }
 
     public int addTemps(double... temps) {
         return 0;
